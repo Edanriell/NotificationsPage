00966a4d08a04599e82d65426292ba6a0bafe841
Edanriell <Edanriell@gmail.com>
2025-02-17 11:55:21 +0200

    feature(notifications-error-message): created styled error message

--- Client/src/entities/notification/ui/notification-list/notification-list.tsx
+++ Client/src/entities/notification/ui/notification-list/notification-list.tsx
@@ -10,20 +10,20 @@ import { Notification } from "../notification";
 
 import styles from "./notification-list.module.css";
 
-function getRandomNumber(min: number = 1, max: number = 100): number {
+// TODO Put in shared
+const getRandomNumber = (min: number = 1, max: number = 100): number => {
 	if (min > max) {
 		throw new Error("Minimum value cannot be greater than maximum value.");
 	}
 
-	// Ensure min and max are integers
 	const adjustedMin = Math.ceil(min);
 	const adjustedMax = Math.floor(max);
 
 	return Math.floor(Math.random() * (adjustedMax - adjustedMin + 1)) + adjustedMin;
-}
+};
 
 export const NotificationList: Component = () => {
-	const [notificationsData] = createResource<SingleNotification[], Error>(fetchNotifications);
+	const [notifications] = createResource<SingleNotification[], Error>(fetchNotifications);
 
 	return (
 		<ul class={styles["notification-list"]}>
@@ -67,13 +67,49 @@ export const NotificationList: Component = () => {
 								<Skeleton width={"80rem"} height={"20rem"} borderRadius={"8rem"} />
 							</div>
 						</div>
+						<div
+							style={{
+								width: "100%",
+								height: "80rem",
+								"border-radius": "8rem",
+								"background-color": "#f7fafd",
+								padding: "18rem 20rem 17rem 20rem",
+								display: "flex",
+								"flex-direction": "row",
+								"column-gap": "19rem",
+								"align-items": "center"
+							}}
+						>
+							<Skeleton width={"45rem"} height={"45rem"} borderRadius={"50%"} />
+							<div
+								style={{
+									display: "flex",
+									"flex-direction": "column",
+									"row-gap": "3rem",
+									"flex-basis": "100%"
+								}}
+							>
+								<Skeleton
+									width={getRandomNumber(25, 95) + "%"}
+									height={"20rem"}
+									borderRadius={"8rem"}
+								/>
+								<Skeleton width={"80rem"} height={"20rem"} borderRadius={"8rem"} />
+							</div>
+						</div>
 					</div>
 				}
 			>
 				<Switch>
-					<Match when={notificationsData.error}>{"Error"}</Match>
-					<Match when={notificationsData()}>
-						<For each={notificationsData()}>
+					<Match when={notifications.error}>
+						<div class={styles["notification-list__error-message-wrapper"]}>
+							<p class={styles["notification-list__error-message"]}>
+								Error: {notifications.error.message}
+							</p>
+						</div>
+					</Match>
+					<Match when={notifications()}>
+						<For each={notifications()}>
 							{(notification) => (
 								<li class={styles["notification-list__item"]}>
 									<Notification notification={notification} />
