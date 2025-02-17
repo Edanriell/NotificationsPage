8b3f3fdd9d1d9b036f6ed2b49bfb54a67341eb7b
Edanriell <Edanriell@gmail.com>
2025-02-17 12:59:35 +0200

    feature(notifications-skeleton): polished notifications skeleton

--- Client/src/entities/notification/ui/notification-skeleton/notification-skeleton.tsx
+++ Client/src/entities/notification/ui/notification-skeleton/notification-skeleton.tsx
@@ -3,30 +3,13 @@ import { Component } from "solid-js";
 import { Skeleton } from "@shared/ui/skeleton/ui";
 import { getRandomNumber } from "@shared/lib/functions";
 
+import styles from "./notification-skeleton.module.css";
+
 export const NotificationSkeleton: Component = () => {
 	return (
-		<div
-			style={{
-				width: "100%",
-				height: "80rem",
-				"border-radius": "8rem",
-				"background-color": "#f7fafd",
-				padding: "18rem 20rem 17rem 20rem",
-				display: "flex",
-				"flex-direction": "row",
-				"column-gap": "19rem",
-				"align-items": "center"
-			}}
-		>
+		<div class={styles["notification-skeleton"]}>
 			<Skeleton width={"45rem"} height={"45rem"} borderRadius={"50%"} />
-			<div
-				style={{
-					display: "flex",
-					"flex-direction": "column",
-					"row-gap": "3rem",
-					"flex-basis": "100%"
-				}}
-			>
+			<div class={styles["notification-skeleton__content"]}>
 				<Skeleton
 					width={getRandomNumber(25, 95) + "%"}
 					height={"20rem"}
